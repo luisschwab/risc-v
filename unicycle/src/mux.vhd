52668cd42ba862f7b31d4f1b7c118e0c92df0f16
Luis Schwab <luisschwab@protonmail.com>
2025-06-23 12:33:19 -0300

    fix(uni): rename `mux3` to `mux4`

--- unicycle/src/mux.vhd
+++ unicycle/src/mux.vhd
@@ -26,7 +26,7 @@ LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.NUMERIC_STD.ALL;
 
-ENTITY mux3 IS
+ENTITY mux4 IS
     GENERIC (
         WIDTH : INTEGER := 32
     );
@@ -34,18 +34,20 @@ ENTITY mux3 IS
         d0 : IN STD_LOGIC_VECTOR(WIDTH - 1 DOWNTO 0);
         d1 : IN STD_LOGIC_VECTOR(WIDTH - 1 DOWNTO 0);
         d2 : IN STD_LOGIC_VECTOR(WIDTH - 1 DOWNTO 0);
+        d3 : IN STD_LOGIC_VECTOR(WIDTH - 1 DOWNTO 0);
         sel : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
         y : OUT STD_LOGIC_VECTOR(WIDTH - 1 DOWNTO 0)
     );
-END ENTITY mux3;
+END ENTITY mux4;
 
-ARCHITECTURE behavioral OF mux3 IS
+ARCHITECTURE behavioral OF mux4 IS
 BEGIN
     WITH sel SELECT y <=
         d0 WHEN "00",
         d1 WHEN "01",
         d2 WHEN "10",
-        d2 WHEN OTHERS;
+        d3 WHEN "11",
+        d3 WHEN OTHERS;
 END ARCHITECTURE behavioral;
 
 --------------------------------------------------------------------------------
@@ -58,7 +60,7 @@ ENTITY mux_tb IS
 END ENTITY mux_tb;
 
 ARCHITECTURE testbench OF mux_tb IS
-    SIGNAL d0_32, d1_32, d2_32, y2_32, y3_32 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+    SIGNAL d0_32, d1_32, d2_32, d3_32, y2_32, y3_32 : STD_LOGIC_VECTOR(31 DOWNTO 0);
     SIGNAL sel_1 : STD_LOGIC;
     SIGNAL sel_2 : STD_LOGIC_VECTOR(1 DOWNTO 0);
 
@@ -71,10 +73,10 @@ ARCHITECTURE testbench OF mux_tb IS
         );
     END COMPONENT;
 
-    COMPONENT mux3 IS
+    COMPONENT mux4 IS
         GENERIC (WIDTH : INTEGER := 32);
         PORT (
-            d0, d1, d2 : IN STD_LOGIC_VECTOR(WIDTH - 1 DOWNTO 0);
+            d0, d1, d2, d3 : IN STD_LOGIC_VECTOR(WIDTH - 1 DOWNTO 0);
             sel : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
             y : OUT STD_LOGIC_VECTOR(WIDTH - 1 DOWNTO 0)
         );
@@ -90,12 +92,13 @@ BEGIN
         y => y2_32
     );
 
-    uut_mux3 : mux3
+    uut_mux4 : mux4
     GENERIC MAP(WIDTH => 32)
     PORT MAP(
         d0 => d0_32,
         d1 => d1_32,
         d2 => d2_32,
+        d3 => d3_32,
         sel => sel_2,
         y => y3_32
     );
@@ -105,8 +108,8 @@ BEGIN
         d0_32 <= x"AAAAAAAA";
         d1_32 <= x"BBBBBBBB";
         d2_32 <= x"CCCCCCCC";
+        d3_32 <= x"DDDDDDDD";
 
-        REPORT "Testing mux2";
         sel_1 <= '0';
         WAIT FOR 10 ns;
         ASSERT y2_32 = x"AAAAAAAA" REPORT "mux2 sel=0 failed" SEVERITY error;
@@ -115,18 +118,21 @@ BEGIN
         WAIT FOR 10 ns;
         ASSERT y2_32 = x"BBBBBBBB" REPORT "mux2 sel=1 failed" SEVERITY error;
 
-        REPORT "Testing mux3";
         sel_2 <= "00";
         WAIT FOR 10 ns;
-        ASSERT y3_32 = x"AAAAAAAA" REPORT "mux3 sel=00 failed" SEVERITY error;
+        ASSERT y3_32 = x"AAAAAAAA" REPORT "mux4 sel=00 failed" SEVERITY error;
 
         sel_2 <= "01";
         WAIT FOR 10 ns;
-        ASSERT y3_32 = x"BBBBBBBB" REPORT "mux3 sel=01 failed" SEVERITY error;
+        ASSERT y3_32 = x"BBBBBBBB" REPORT "mux4 sel=01 failed" SEVERITY error;
 
         sel_2 <= "10";
         WAIT FOR 10 ns;
-        ASSERT y3_32 = x"CCCCCCCC" REPORT "mux3 sel=10 failed" SEVERITY error;
+        ASSERT y3_32 = x"CCCCCCCC" REPORT "mux4 sel=10 failed" SEVERITY error;
+
+        sel_2 <= "11";
+        WAIT FOR 10 ns;
+        ASSERT y3_32 = x"DDDDDDDD" REPORT "mux4 sel=11 failed" SEVERITY error;
 
         WAIT;
     END PROCESS stimulus;
